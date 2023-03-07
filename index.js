9eb1a301bd8fa9779c0825768cf0885ebba9e090
Sarika Khatri <sarikakhatri142000@gmail.com>
2023-03-07 21:02:19 +0530

    linking static files using layouts

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express= require('express');
 const app=express();
 const port= 8000;
-
+const expressLayouts= require('express-ejs-layouts');
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+//extract layouts and scrips from subpages to the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 // use express router
 app.use('/', require('./routes'));
 //set up the view engine
