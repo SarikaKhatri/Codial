53a5d7396784366434cb8fabfd050423b99bbe53
Sarika Khatri <sarikakhatri142000@gmail.com>
2023-03-07 22:03:38 +0530

    creating schema

--- routes/users.js
+++ routes/users.js
@@ -2,4 +2,6 @@ const express= require('express');
 const router= express.Router();
 const usersController= require('../controllers/Users_Conroller');
 router.get('/profile',usersController.profile);
+router.get('/sign-up',usersController.signUp);
+router.get('/sign-in',usersController.signIn);
 module.exports=router;
